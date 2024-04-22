3a570849104579d1d7b29367ba6c66210088497e
shahbaz42 <shahbaz_ali@outlook.in>
2024-04-23 04:42:25 +0530

    setup express

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,11 @@
-console.log(3);
+import express from "express";
+import ServerConfig from "./config/serverConfig";
+import http from "http";
+
+const app = express();
+
+const server = http.createServer(app);
+
+server.listen(ServerConfig.PORT, () => {
+    console.log(`Server is running on port ${ServerConfig.PORT}`);
+});
