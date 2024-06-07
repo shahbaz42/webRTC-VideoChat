1d72e22a666229d274167671d31f97cc17bee871
Shahbaz Ali <shahbaz_ali@outlook.in>
2024-06-08 00:50:03 +0530

    separate out express server and peerjs server

--- server/src/config/serverConfig.ts
+++ server/src/config/serverConfig.ts
@@ -2,6 +2,9 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+console.log(process.env.PORT);
+
 export default {
-    PORT: process.env.PORT || 5000,
+    PORT: process.env.PORT || 8007,
+    PEER_PORT: process.env.PEER_PORT || 8008,
 };
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,9 @@
 import express from "express";
+import peerExpress from "express";
 import ServerConfig from "./config/serverConfig";
 import { Server } from "socket.io";
 import http from "http";
-import cors from 'cors';
+import cors from "cors";
 import roomHandler from "./handlers/roomHandler";
 import { ExpressPeerServer } from "peer";
 
@@ -14,29 +15,42 @@ app.enable("trust proxy");
 const server = http.createServer(app);
 
 const io = new Server(server, {
-    cors: {
-        origin: '*',
-        methods: ['GET', 'POST']
-    }
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
 });
 
 io.on("connection", (socket) => {
-    console.log("A new user has connected");
+  console.log("A new user has connected");
 
-    roomHandler(socket);
+  roomHandler(socket);
 
-    socket.on("disconnect", () => {
-        console.log("A user disconnected");
-    });
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
+  });
+
+  socket.on("error", (error) => {
+    console.log("An error occurred: ", error);
+  });
 });
 
-const peerServer = ExpressPeerServer(server, {
-	path: "/",
-});
-
-app.use("/myapp", peerServer);
-
+// Express server
 server.listen(ServerConfig.PORT, () => {
-    console.log(`Server is running on port ${ServerConfig.PORT}`);
+  console.log(`Server is running on port ${ServerConfig.PORT}`);
 });
+
+// Peer server
+const peerApp = peerExpress();
+const peerHttpServer = http.createServer(peerApp);
+const peerServer = ExpressPeerServer(peerHttpServer, {
+    path: "/",
+});
+
+peerApp.use("/peerjs", peerServer);
+peerHttpServer.listen(ServerConfig.PEER_PORT, () => {
+    console.log(`Peer server is running on port ${ServerConfig.PEER_PORT}`);
+});
+
+
 
